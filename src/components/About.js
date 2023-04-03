ed975e86ab44cc51afb8938f9d1f8f638922500a
aryanGupta-09 <aryangupta0329@gmail.com>
2023-04-03 14:41:13 +0530

    improve about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { svgpaths, svgcolors } from "../data";
 import { motion, useMotionValue, useTransform, animate } from "framer-motion";
 import { getIndex, useFlubber } from "./use-flubber";
@@ -27,27 +27,43 @@ function About() {
         return () => animation.stop();
     }, [pathIndex, progress]);
 
-    const styles = {
-        section: { backgroundColor: "#080823" }
-    }
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const mediaQuery = window.matchMedia("(max-width: 1000px)");
+
+        setIsMobile(mediaQuery.matches);
+
+        const handleMediaQueryChange = (event) => {
+            setIsMobile(event.matches);
+        };
+
+        mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+        return () => {
+            mediaQuery.removeEventListener("change", handleMediaQueryChange);
+        };
+    }, []);
 
     return (
-        <section className="d-flex justify-content-center" style={styles.section}>
-            <div style={{ marginLeft: "100px", marginTop: "30px" }}>
-                <h1 className="text-white">About Me</h1>
-                <div className="text-white" style={{ marginTop: "20px", width: "70%" }}>
-                    I'm a full stack developer and have experience in developing web applications using ReactJS, NodeJS, ExpressJS, MongoDB, and MySQL.
-                    I'm also a competitive programmer and have participated in various coding competitions like Google Kick Start and Meta Hacker Cup. <br/>
-                    I'm currently pursuing my Bachelors in Computer Science and Design from IIIT-Delhi.
+        <section style={{ backgroundColor: "#080823", paddingLeft: isMobile ? "7%" : "22%", paddingRight: isMobile ? "7%" : "22%", paddingTop: "70px", paddingBottom: "70px" }}>
+            <div className="d-flex justify-content-center gap-5 rounded" style={{backgroundColor: "#1F1F1F"}}>
+                <div style={{ marginTop: "30px", paddingLeft: "50px", width: isMobile ? "" : "50%" }}>
+                    <h1 className="text-white">About Me</h1>
+                    <div className="text-white" style={{ marginTop: "20px" }}>
+                        I'm a full stack developer and have experience in developing web applications using ReactJS, NodeJS, ExpressJS, MongoDB, and MySQL. <br />
+                        I'm also a competitive programmer and have participated in various coding competitions like Google Kick Start and Meta Hacker Cup. <br />
+                        I'm currently pursuing my Bachelors in Computer Science and Design from <span>IIIT-Delhi</span>.
+                    </div>
+                    <br />
                 </div>
-                <br />
-            </div>
-            <div style={{ marginTop: "60px"}}>
-                <svg width="400" height="400">
-                    <g transform="scale(10 10)">
-                        <motion.path fill={fill} d={path} />
-                    </g>
-                </svg>
+                <div style={{ marginTop: "40px" }}>
+                    <svg width="230" height="250">
+                        <g transform="translate(10 10) scale(10 10)">
+                            <motion.path fill={fill} d={path} />
+                        </g>
+                    </svg>
+                </div>
             </div>
         </section>
     );
