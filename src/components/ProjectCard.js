3bea0c86285d07cbcbff89dcc3a5377aa6c53420
aryanGupta-09 <aryangupta0329@gmail.com>
2023-04-03 14:52:13 +0530

    change color of card title

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -9,11 +9,11 @@ const ProjectCard = ({project}) => {
             <Card className="rounded" style={{backgroundColor: "#1F1F1F", width: "350px", padding: "10px", paddingTop: "15px"}}>
                 <Card.Img variant="top" src={project.image} alt={project.title} className="rounded" style={{maxWidth: "100%", height: "270px"}}/>
                 <Card.Body>
-                    <Card.Title className="text-white text-center">{project.title}</Card.Title>
+                    <Card.Title className="text-center" style={{color: "#7449d5"}}>{project.title}</Card.Title>
                     <Card.Text className="text-white text-center">
                         {project.description}
                     </Card.Text>
-                    <Card.Link href={project.link} style={{marginLeft: "140px"}}><img src={github} alt="github-link" className="bg-white rounded-circle" style={{height: "30px", width: "30px"}}/></Card.Link>
+                    <Card.Link href={project.link} style={{marginLeft: "135px"}}><img src={github} alt="github-link" className="bg-white rounded-circle" style={{height: "30px", width: "30px"}}/></Card.Link>
                 </Card.Body>
             </Card>
         </Tilt>
